004d6deedabd9a8853ebc481584981b95c65ae27
yedio <yediobjet@gmail.com>
2022-03-01 17:17:49 +0900

    init-setting

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Main() {
+  return <Wrapper>MAIN</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
